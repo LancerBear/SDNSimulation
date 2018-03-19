b8a4b7cd0826a95996fad673dbc11b6991da81da
LancerBear <lancer_bear@163.com>
2018-03-19 22:22:02 +0800

    the first version, only FileReader is functioning

---
+++ Switch/Switch/FileReader.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Switch
+{
+	public static class FileReader
+	{
+		/// <summary>
+		/// 从文件中初始化拓扑结构
+		/// </summary>
+		/// <param name="fileName"></param>
+		/// <returns></returns>
+		public static RetVal.EN_RET_CODE InitFromFile(string fileName)
+		{
+			XmlDocument doc = new XmlDocument();
+			try
+			{
+				doc.Load(fileName);
+			}catch(Exception ex)
+			{
+				Util.Log(Util.EN_LOG_LEVEL.EN_LOG_FATAL, fileName + "拓扑结构文件不存在！");
+				return RetVal.EN_RET_CODE.EN_RET_FILE_NOT_EXIST;
+			}
+
+			Util.Log(Util.EN_LOG_LEVEL.EN_LOG_INFO, "读取拓扑文件");
+
+			try
+			{
+				XmlElement root = doc.DocumentElement;
+				XmlNodeList pairList = root.GetElementsByTagName("pair");
+
+				foreach (XmlNode PairNode in pairList)
+				{
+					XmlNodeList switchList = ((XmlElement)PairNode).GetElementsByTagName("switch");
+					string strID0 = ((XmlElement)switchList[0]).GetElementsByTagName("id")[0].InnerText;
+					string strID1 = ((XmlElement)switchList[1]).GetElementsByTagName("id")[0].InnerText;
+					string strPhyPortNo;
+					string strLocalPortNum;
+					string strRemotePortNum;
+
+					if (strID0 == Program.switchID.ToString())
+					{
+						strPhyPortNo = ((XmlElement)switchList[0]).GetElementsByTagName("PhyPortNo")[0].InnerText;
+						strLocalPortNum = ((XmlElement)switchList[0]).GetElementsByTagName("LocalPortNum")[0].InnerText;
+						strRemotePortNum = ((XmlElement)switchList[1]).GetElementsByTagName("LocalPortNum")[0].InnerText;
+					}
+					else if (strID1 == Program.switchID.ToString())
+					{
+						strPhyPortNo = ((XmlElement)switchList[1]).GetElementsByTagName("PhyPortNo")[0].InnerText;
+						strLocalPortNum = ((XmlElement)switchList[1]).GetElementsByTagName("LocalPortNum")[0].InnerText;
+						strRemotePortNum = ((XmlElement)switchList[0]).GetElementsByTagName("LocalPortNum")[0].InnerText;
+					}
+					else
+					{
+						continue;
+					}
+					Console.WriteLine("PhyPortNo = {0}, LocalPortNum = {1}, RemotePortNum = {2}", strPhyPortNo, strLocalPortNum, strRemotePortNum);
+				}
+			}catch(Exception ex)
+			{
+				Util.Log(Util.EN_LOG_LEVEL.EN_LOG_FATAL, "Xml格式错误");
+			}
+			return RetVal.EN_RET_CODE.EN_RET_SUCC;
+		}
+	}
+}
---
+++ Switch/Switch/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Switch
+{
+	class Program
+	{
+		public const int MAX_SWITCH_NUM = 127;
+		public const int MIN_SWITCH_NUM = 0;
+		public static int switchID = -1;
+		static void Main(string[] args)
+		{
+			Util.Log(Util.EN_LOG_LEVEL.EN_LOG_INFO, "系统启动");
+			if (0 == args.Length)
+			{
+				Util.Log(Util.EN_LOG_LEVEL.EN_LOG_FATAL, "输入参数不存在");
+				Environment.Exit(0);
+			}
+			
+			if (!int.TryParse(args[0], out switchID))
+			{
+				Util.Log(Util.EN_LOG_LEVEL.EN_LOG_FATAL, "输入参数格式不正确");
+				Environment.Exit(0);
+			}
+
+			if (switchID > MAX_SWITCH_NUM || switchID < MIN_SWITCH_NUM)
+			{
+				Util.Log(Util.EN_LOG_LEVEL.EN_LOG_FATAL, "交换机ID过大或过小");
+				Environment.Exit(0);
+			}
+
+			Util.Log(Util.EN_LOG_LEVEL.EN_LOG_INFO, "交换机ID: " + switchID.ToString());
+
+			RetVal.EN_RET_CODE retVal = RetVal.EN_RET_CODE.EN_RET_INIT;
+			
+			//进行系统初始化工作，包括建立拓扑等
+			retVal = SystemInit();
+			if (retVal != RetVal.EN_RET_CODE.EN_RET_SUCC)
+			{
+				Util.Log(Util.EN_LOG_LEVEL.EN_LOG_FATAL, "初始化失败");
+				Environment.Exit(0);
+			}
+		}
+
+		/// <summary>
+		/// 系统初始化
+		/// </summary>
+		/// <returns></returns>
+		private static RetVal.EN_RET_CODE SystemInit()
+		{
+			RetVal.EN_RET_CODE retVal = RetVal.EN_RET_CODE.EN_RET_INIT;
+			retVal = FileReader.InitFromFile("topology.xml");
+			if (RetVal.EN_RET_CODE.EN_RET_SUCC != retVal)
+			{
+				return retVal;
+			}
+			return RetVal.EN_RET_CODE.EN_RET_SUCC;
+		}
+	}
+}
