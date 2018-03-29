efad0ed83c9c8ca9388d3411edd6434b631592b6
LancerBear <lancer_bear@163.com>
2018-03-29 18:21:32 +0800

    增加控制器程序的应用层类ControllerApp，循环读消息队列，计划下一步将控制器、交换机中共用的文件提取出，新建一个项目，控制器和交换机项目都引用这个项目

---
+++ Switch/Controller/AppLayer/ControllerApp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+	public static class ControllerApp
+	{
+		public static Const.EN_RET_CODE Init()
+		{
+			return Const.EN_RET_CODE.EN_RET_SUCC;
+		}
+
+		/// <summary>
+		/// SDN业务开始，函数内死循环，读消息队列后处理
+		/// </summary>
+		public static void StartApp()
+		{
+			Packet packet = null;
+			while (true)
+			{
+				packet = null;
+
+				//P操作
+				Program.PktQueueMutex.WaitOne();
+				//消息队列为空，进行V操作之后继续循环读队列
+				if (Program.PacketQueue.Count == 0)
+				{
+					Program.PktQueueMutex.ReleaseMutex();
+					continue;
+				}
+				//消息队列不为空，消息包出队后进行V操作
+				packet = Program.PacketQueue.Dequeue();
+				Program.PktQueueMutex.ReleaseMutex();
+
+				
+			}
+		}
+
+
+		public static void DealReceivePacket()
+		{
+
+		}
+	}
+}
